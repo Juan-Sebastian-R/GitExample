66c5bfca5c6960f2464fa0fcc6eb2bcd9a0662b5
Juan-Sebastian-R <jsgallardo@uninorte.edu.co>
2025-03-21 15:38:19 -0500

    Feat: Added Person Class

--- src/main/Main.java
+++ src/main/Main.java
@@ -4,12 +4,15 @@
  */
 package main;
 
+import core.Person;
+
 /**
  *
  * @author jsgallardo
  */
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");
+        Person person = new Person(1548845899L, "Juan Perez", 23);
+        System.out.println(person);
     }
 }
