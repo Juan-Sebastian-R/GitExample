a209ebc00396576ab9b47f3300c81807d1555905
Juan-Sebastian-R <jsgallardo@uninorte.edu.co>
2025-03-21 16:01:12 -0500

    Feat: Add Cat class

---
+++ src/core/Cat.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package core;
+
+/**
+ *
+ * @author Juan Sebastian
+ */
+public class Cat {
+    private String name;
+    private int age;
+    private String color;
+
+    public Cat(String name, int age, String color) {
+        this.name = name;
+        this.age = age;
+        this.color = color;
+    }
+}
